ea06b4f5d27d538f5fbc62d154d3d06b0a08baaa
Narra Greeshma Siri <greeshmasreenarra@gmail.com>
2025-10-31 10:51:57 +0530

    Update Sequencer.sv

--- AHB Lite Protocol/Sequencer.sv
+++ AHB Lite Protocol/Sequencer.sv
@@ -1,4 +1,4 @@
-class ahb_lite_sequencer extends uvm_sequencer #(trans);
+class ahb_lite_sequencer extends uvm_sequencer #(ahb_lite_seq_item);
     `uvm_component_utils(ahb_lite_sequencer)
     
     function new(string name, uvm_component parent);
