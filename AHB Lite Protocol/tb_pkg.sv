3415e491294562a8d285b9334b3328e5368402af
Narra Greeshma Siri <greeshmasreenarra@gmail.com>
2025-10-31 10:27:13 +0530

    Create tb_pkg.sv

---
+++ AHB Lite Protocol/tb_pkg.sv
@@ -0,0 +1,12 @@
+//`include "uvm_macros.svh" 
+package tb_pkg;
+  `include "Seq_item.sv"
+  `include "Sequence.sv"
+  `include "Sequencer.sv"
+  `include "Driver.sv"
+  `include "Monitor.sv"
+  `include "Agent.sv"
+  `include "Scoreboard.sv"
+  `include "Coverage.sv"
+  `include "Environment.sv"
+endpackage
